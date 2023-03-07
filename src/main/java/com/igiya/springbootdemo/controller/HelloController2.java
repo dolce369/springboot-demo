50430f5fa9f880cb5a1e6adf12ce110ce5adfbb9
Peter Han <phan@Peters-MacBook-Air.local>
2023-03-07 16:55:03 -0500

    testing from mac os

--- src/main/java/com/igiya/springbootdemo/controller/HelloController2.java
+++ src/main/java/com/igiya/springbootdemo/controller/HelloController2.java
@@ -2,12 +2,14 @@ package com.igiya.springbootdemo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HelloController2 {
     
     @GetMapping("/hello2")
-    public String hello2() {
-        return "hello2*** hello just for testing purpose";
+    public ResponseEntity<String> hello2() {
+
+        return ResponseEntity.ok("hello2*** hello just for testing purpose");
     }
 }
